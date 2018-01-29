55f64fc39e8fb0d3a124a6ad5655c5d325be2148
CJ Cenizal <cj@cenizal.com>
2018-01-28 16:45:41 -0800

    Inline default renderer test values instead of using snapshots.

--- src/components/value_renderer/default_renderer/default_renderer.test.js
+++ src/components/value_renderer/default_renderer/default_renderer.test.js
@@ -4,31 +4,31 @@ describe('Value Renderer', () => {
   describe('defaultRenderer', () => {
 
     test('boolean value', () => {
-      expect(defaultRenderer(true)).toMatchSnapshot();
-      expect(defaultRenderer(false)).toMatchSnapshot();
+      expect(defaultRenderer(true)).toBe('Yes');
+      expect(defaultRenderer(false)).toBe('No');
     });
 
     test('numeric value', () => {
-      expect(defaultRenderer(1234.567)).toMatchSnapshot();
+      expect(defaultRenderer(1234.567)).toBe('1234.567');
     });
 
     test('string value', () => {
-      expect(defaultRenderer('value')).toMatchSnapshot();
+      expect(defaultRenderer('value')).toBe('value');
     });
 
     test('date value', () => {
       const value = new Date(1999, 0, 1, 2, 3, 4, 5);
-      expect(defaultRenderer(value)).toMatchSnapshot();
+      expect(defaultRenderer(value)).toBe('1 Jan 1999 02:03');
     });
 
     test('array of dates', () => {
       const dates = [ new Date(1999, 0, 1, 2, 3, 4, 5) ];
-      expect(defaultRenderer(dates)).toMatchSnapshot();
+      expect(defaultRenderer(dates)).toBe('1 Jan 1999 02:03');
     });
 
     test('object value', () => {
       const obj = { key: 'value' };
-      expect(defaultRenderer(obj)).toMatchSnapshot();
+      expect(defaultRenderer(obj)).toBe(`{\"key\":\"value\"}`);
     });
 
   });
